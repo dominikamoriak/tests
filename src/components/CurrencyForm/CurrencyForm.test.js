cab65e9f3ccd1fb722fe4b1280c86a6eb5acbe26
Dominika Moriak <dominika.moriak@gmail.com>
2024-03-20 16:07:40 +0100

    Add new test-should run action callback with proper data on form submit

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,8 +1,23 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CurrencyForm from './CurrencyForm';
+import userEvent from '@testing-library/user-event';
 
 describe('Component CurrencyForm', () => {
   it('should render without crashing', () => {
     render(<CurrencyForm action={() => {}} />);
+  });
+  it('should run action callback with proper data on form submit', () => {
+    const action = jest.fn();
+    // render component
+    render(<CurrencyForm action={action} />);
+
+    //find "convert" button
+    const submitButton = screen.getByText('Convert');
+
+    //simulate user click on "convert" button
+    userEvent.click(submitButton);
+
+    //check if action callback was called once
+    expect(action).toHaveBeenCalledTimes(1);
   });
 });
