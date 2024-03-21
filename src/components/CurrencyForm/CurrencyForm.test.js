a3e1a12fd3590046b91e34012bc6d114747c04a5
Dominika Moriak <dominika.moriak@gmail.com>
2024-03-21 12:02:31 +0100

    Add new code to the last test

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -8,16 +8,28 @@ describe('Component CurrencyForm', () => {
   });
   it('should run action callback with proper data on form submit', () => {
     const action = jest.fn();
-    // render component
+
+    //render component
     render(<CurrencyForm action={action} />);
 
     //find "convert" button
     const submitButton = screen.getByText('Convert');
 
+    //find fields elems
+    const amountField = screen.getByTestId('amount');
+    const fromField = screen.getByTestId('from-select');
+    const toField = screen.getByTestId('to-select');
+
+    //set test values to fields
+    userEvent.type(amountField, '100');
+    userEvent.selectOptions(fromField, 'PLN');
+    userEvent.selectOptions(toField, 'USD');
+
     //simulate user click on "convert" button
     userEvent.click(submitButton);
 
-    //check if action callback was called once
+    //check if action callback was called once and with proper argument
     expect(action).toHaveBeenCalledTimes(1);
+    expect(action).toHaveBeenCalledWith({ amount: 100, from: 'PLN', to: 'USD' });
   });
 });
