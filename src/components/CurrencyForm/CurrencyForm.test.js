23f94e1d68aa2c578fb75140e3dbc9eead2c714f
Dominika Moriak <dominika.moriak@gmail.com>
2024-03-22 13:56:32 +0100

    Add array testCases to the last test and use it

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import CurrencyForm from './CurrencyForm';
 import userEvent from '@testing-library/user-event';
 
@@ -7,6 +7,15 @@ describe('Component CurrencyForm', () => {
     render(<CurrencyForm action={() => {}} />);
   });
   it('should run action callback with proper data on form submit', () => {
+    const testCases = [
+      { amount: '100', from: 'PLN', to: 'USD' },
+      { amount: '20', from: 'USD', to: 'PLN' },
+      { amount: '200', from: 'PLN', to: 'USD' },
+      { amount: '345', from: 'USD', to: 'PLN' },
+    ];
+    
+    for(const testObj of testCases) {
+
     const action = jest.fn();
 
     //render component
@@ -21,15 +30,23 @@ describe('Component CurrencyForm', () => {
     const toField = screen.getByTestId('to-select');
 
     //set test values to fields
-    userEvent.type(amountField, '100');
-    userEvent.selectOptions(fromField, 'PLN');
-    userEvent.selectOptions(toField, 'USD');
+    userEvent.type(amountField, testObj.amount);
+    userEvent.selectOptions(fromField, testObj.from);
+    userEvent.selectOptions(toField, testObj.to);
 
     //simulate user click on "convert" button
     userEvent.click(submitButton);
 
     //check if action callback was called once and with proper argument
     expect(action).toHaveBeenCalledTimes(1);
-    expect(action).toHaveBeenCalledWith({ amount: 100, from: 'PLN', to: 'USD' });
+    expect(action).toHaveBeenCalledWith({ 
+      amount: parseInt(testObj.amount, 10),
+      from: testObj.from,
+      to: testObj.to
+     });
+
+    //unmount component
+    cleanup();
+    }
   });
 });
