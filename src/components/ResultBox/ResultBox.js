a3960e5519a0dd32778f5b555e2af10a9312a4ba
Dominika Moriak <dominika.moriak@gmail.com>
2024-03-23 12:21:26 +0100

    Add new test about conversion from PLN to USD

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -16,7 +16,7 @@ const ResultBox = ({ from, to, amount }) => {
   const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
 
   return (
-    <div className={styles.result}>
+    <div data-testid="output" className={styles.result}>
       {formattedAmount} = {convertedAmount}
     </div>
   );
--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,8 +1,19 @@
 import ResultBox from './ResultBox';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Component ResultBox', () => {
     it('should render without crashing', () => {
         render(<ResultBox from="PLN" to="USD" amount={100} />);
+    });
+    it('should render proper info about conversion when PLN -> USD', () => {
+         //render component
+         render(<ResultBox from="PLN" to="USD" amount={100} />)
+
+         //find main div
+         const output = screen.getByTestId('output');
+
+         //check if the main div has the right content
+         expect(output).toHaveTextContent('PLN 100.00 = $28.57');
     });
 });
