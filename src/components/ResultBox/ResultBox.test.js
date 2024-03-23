df8f47f76902d5ea7e4b88cbe84b4cfec789c548
Dominika Moriak <dominika.moriak@gmail.com>
2024-03-23 14:32:59 +0100

    Add array to the last test and use it

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,5 +1,5 @@
 import ResultBox from './ResultBox';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Component ResultBox', () => {
@@ -7,13 +7,26 @@ describe('Component ResultBox', () => {
         render(<ResultBox from="PLN" to="USD" amount={100} />);
     });
     it('should render proper info about conversion when PLN -> USD', () => {
+        const testCases = [
+            { amount: 100, expected: 'PLN 100.00 = $28.57' },
+            { amount: 350, expected: 'PLN 350.00 = $100.00' },
+            { amount: 50, expected: 'PLN 50.00 = $14.29' },
+            { amount: 0, expected: 'PLN 0.00 = $0.00' },
+        ];
+
+        for(const testObj of testCases) {
+
          //render component
-         render(<ResultBox from="PLN" to="USD" amount={100} />)
+         render(<ResultBox from="PLN" to="USD" amount={testObj.amount} />)
 
          //find main div
          const output = screen.getByTestId('output');
 
          //check if the main div has the right content
-         expect(output).toHaveTextContent('PLN 100.00 = $28.57');
+         expect(output).toHaveTextContent(testObj.expected);
+
+         //unmount component
+         cleanup();
+        }
     });
 });
